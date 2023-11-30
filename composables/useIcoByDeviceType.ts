d0592af04b06dc9362a5eae51ef6fe68a68bcb1d
anythingelse428 <vladn803@gmail.com>
2023-11-30 16:11:56 +0400

    1

---
+++ composables/useIcoByDeviceType.ts
@@ -0,0 +1,22 @@
+export default function useIcoByDeviceType (type:string) {
+  if (!type) { return }
+  if (type.includes('devices.types.sensor')) {
+    return { name: 'access-point', code: "\\F0003" }
+  }
+  switch (type) {
+    case 'devices.types.switch':
+      return { name: 'power-plug-outline', code: '\\F1425' }
+    case 'devices.types.openable.lock':
+      return { name: 'lock-outline', code: '\\F0341' }
+    case 'devices.types.light':
+      return { name: 'lightbulb-variant-outline', code: '\\F1803' }
+    case 'devices.types.thermostat':
+      return { name: 'home-thermometer-outline', code: '\\F0F55' }
+    case 'devices.types.openable.valve':
+      return { name: 'water-outline', code: '\\F0E0A' }
+    case 'devices.types.openable.garage':
+      return { name: 'garage-variant', code: '\\F12D3' }
+    default:
+      return { name: 'block-helper', code: '\\F00AD' }
+  }
+}
