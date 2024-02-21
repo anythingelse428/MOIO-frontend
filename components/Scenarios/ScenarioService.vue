00ee94c73e05a625f13bb40d00178308a0c4549b
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2024-02-21 18:10:24 +0400

    фиксы

--- components/Scenarios/ScenarioService.vue
+++ components/Scenarios/ScenarioService.vue
@@ -90,7 +90,7 @@ const isDeleteModalShow = ref(false)
 const groupStore = useGroupsStore()
 
 onClickOutside(target, (event) => {
-  if (event.target?.className && event.target?.className?.includes('modal__content')) {
+  if (event.target?.className && event.target?.className?.indexOf('modal__content') > -1) {
     isCapabilitiesShow.value = false
     isDeleteModalShow.value = false
   }
