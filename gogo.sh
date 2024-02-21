cddc740c373587260f3ff1c6a35bf680cd32e854
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2024-02-21 17:19:27 +0400

    Скрипт для обновления сборки и запуска прода

---
+++ gogo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Остановить процесс nuxt-prod в PM2
+pm2 stop nuxt-prod
+
+# Выполнить git pull для обновления кода
+git pull
+
+# Собрать проект и запустить его с помощью PM2
+npm run build && pm2 start ecosystem.config.cjs --only nuxt-prod
