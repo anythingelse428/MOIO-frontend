07a7775ce77ab24116257b6d3a4f6c89941c0161
anythingelse428 <68991138+anythingelse428@users.noreply.github.com>
2024-03-20 15:49:48 +0400

    steve jobs moment

--- components/Automation/AutomationCondition.vue
+++ components/Automation/AutomationCondition.vue
@@ -42,7 +42,9 @@
           {{ timeOffset ? timeOffset[0] : '' }}
         </div>
         <div class="automation-condition__time-value">
-          <span v-if="!editable" class="time">{{ timeRange?.startTime ?? 'err' }}</span>
+          <span v-if="!editable" class="time">
+            {{ timeRange?.startTime ?? 'err' }}
+          </span>
           <input
             v-if="editable"
             ref="timeInput"
@@ -54,7 +56,9 @@
             @keydown.enter.prevent="false"
           >
           -
-          <span v-if="!editable" class="time">{{ timeRange?.endTime ?? 'err' }}</span>
+          <span v-if="!editable" class="time">
+            {{ timeRange?.endTime ?? 'err' }}
+          </span>
           <input
             v-if="editable"
             ref="timeInput"
